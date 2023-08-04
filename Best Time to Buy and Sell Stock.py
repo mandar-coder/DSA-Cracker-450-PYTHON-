91ba593681e802e5b342ebdcaefdee0157a189c2
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-04 10:42:40 +0530

    Day 20

---
+++ Best Time to Buy and Sell Stock.py
@@ -0,0 +1,14 @@
+#Best Time to Buy and Sell Stock
+def maxprofit(arr, n):
+    buy = arr[0]
+    max = 0
+    for i in range(1,n):
+        if(buy>arr[i]):
+            buy = arr[i]
+        elif(arr[i]-buy>max):
+            max = arr[i]-buy
+    return max
+    
+arr = [7, 1, 5, 6, 4]
+n= len(arr)
+print("max profit", maxprofit(arr,n))
