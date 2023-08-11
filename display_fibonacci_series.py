a69f7a6a66ca312f5f67924399753685e1139c99
Mandar <78254472+mandar-coder@users.noreply.github.com>
2023-08-11 13:50:02 +0530

    Day 1

--- display_fibonacci_series.py
+++ display_fibonacci_series.py
@@ -1,18 +1,28 @@
-#display_fibonacci_series
+# Program to display the Fibonacci series
+
+# Function to generate and print the Fibonacci series
 def fibo(a):
-	m,n = 0,1
-	count = 0
-	if a<=0:
-		print("Alert!")
-	elif a == 1:
-		print(f"fibonaci:{m}")
-	else:
-		print(f"fibonaci: ",end=" ")
-	while count < a:
-		count +=1
-		print (m, end=" ")
-		nth = m+n
-		m=n 
-		n = nth
-a= int(input("How Many no u want to display:"))
+    m, n = 0, 1
+    count = 0
+
+    # Check if the input is valid
+    if a <= 0:
+        print("Alert!")
+    elif a == 1:
+        print(f"Fibonacci: {m}")
+    else:
+        print("Fibonacci:", end=" ")
+
+    # Generate and print the Fibonacci series
+    while count < a:
+        count += 1
+        print(m, end=" ")
+        nth = m + n
+        m = n
+        n = nth
+
+# Get user input for the number of Fibonacci numbers to display
+a = int(input("How many numbers do you want to display: "))
 fibo(a)
+
+# Code by Mandar
